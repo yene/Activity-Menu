cd95ea83ee51be63db16138f9a87f1c450999228
yene <yene@cocoadojo.com>
2015-06-21 18:33:13 +0200

    Initial Commit

---
+++ Activity Menu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Activity Menu
+//
+//  Created by Yannick Weiss on 21/06/15.
+//  Copyright (c) 2015 Yannick Weiss. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Activity Menu/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Activity Menu
+//
+//  Created by Yannick Weiss on 21/06/15.
+//  Copyright (c) 2015 Yannick Weiss. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
